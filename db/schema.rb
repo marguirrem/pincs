516b70200ef1e0f495760e6ee7bfedbf8a398032
Marlon <muekka403@gmail.com>
2016-03-19 22:21:38 -0600

    Agregamos estilos a los formularios de registro y de sesion, agregamos el campo nombre a los usuarios,link del nombre con opciones agregado

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319231249) do
+ActiveRecord::Schema.define(version: 20160320035512) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "photo"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160319231249) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
